3a3ac32526a3d4f362097172b61bdf50ebf50fb3
masaki-yosida <yashukasono@gmail.com>
2023-11-07 20:06:51 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,7 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
   #has_many :users, through: :room_users中間テーブルを使うのに必要
+
+  validates :name, presence: true
+
 end
